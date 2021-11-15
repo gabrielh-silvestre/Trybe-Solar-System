276ffdc2e650b777fdfb20f2502056f1a936c6ce
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-15 16:01:48 -0300

    feat(Title): add custom classes from props

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 
 export default class Title extends Component {
   render() {
-    const { headline } = this.props;
+    const { headline, className } = this.props;
 
-    return <h2>{headline}</h2>;
+    return <h2 className={className}>{headline}</h2>;
   }
 }
 
 Title.propTypes = {
   headline: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
 };
