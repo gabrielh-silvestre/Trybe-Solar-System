f7234c2537c26a44f9f732d1574ce4bf84eee638
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-15 15:45:59 -0300

    style(Header): customize component for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <header>
-        <h1>Sistema Solar</h1>
+      <header className="w-full bg-gradient-header text-center py-5">
+        <h1 className="text-2xl font-bold">Sistema Solar</h1>
       </header>
     );
   }
