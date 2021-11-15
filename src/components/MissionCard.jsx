f52a67bab4b2e4383c17d17d451153f5104e3c0e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-15 16:38:30 -0300

    style(components): implement large screen responsivity

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -6,7 +6,7 @@ export default class MissionCard extends Component {
     const { name, year, country, destination } = this.props;
 
     return (
-      <section className="m-4 py-4 text-center shadow-md rounded-lg" data-testid="mission-card">
+      <section className="m-4 py-4 text-center shadow-md rounded-lg lg:px-2" data-testid="mission-card">
         <h3 className="text-lg" data-testid="mission-name">{name}</h3>
         <p className="py-1" data-testid="mission-year">{year}</p>
         <p className="py-1" data-testid="mission-country">{country}</p>
