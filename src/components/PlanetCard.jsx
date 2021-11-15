c08a3ef8b725024859a93cb85b5d6317ca948590
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-15 16:21:45 -0300

    style(PlanetCard): center image

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -8,7 +8,7 @@ export default class PlanetCard extends Component {
     return (
       <section className="m-4 text-center" data-testid="planet-card">
         <h3 className="text-lg" data-testid="planet-name">{planetName}</h3>
-        <img className="bg-gradient-planetCard py-6" src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <img className="bg-gradient-planetCard py-6 mx-auto" src={ planetImage } alt={ `Planeta ${planetName}` } />
       </section>
     );
   }
