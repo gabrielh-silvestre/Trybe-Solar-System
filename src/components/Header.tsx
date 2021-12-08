e4c99b66d0b524476820722b50301bb0370a5ba3
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-08 13:21:04 -0300

    refactor(Header): change JS class component to TS funtional component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="cursor-default w-full bg-gradient-header text-center py-5">
+      <h1 className="text-2xl font-bold">Sistema Solar</h1>
+    </header>
+  );
+}
